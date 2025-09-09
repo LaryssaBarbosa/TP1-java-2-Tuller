1d95bfbf27754d420a2f7ead379c7bbf89e5f5e7
Laryssa Barbosa <168648324+LaryssaBarbosa@users.noreply.github.com>
2025-09-08 23:43:26 -0300

    Update TestBook.java

--- TestBook.java
+++ TestBook.java
@@ -1,4 +1,22 @@
 //ex2 Laryssa Barbosa Soares - CB3031667
+
+/*
+Um livro pode ser escrito por um ou muitos autores, por esta razão a classe Book deve
+ter um array de autores, conforme o modelo abaixo:
+
+- O construtor deve receber um array de autores
+- Uma vez que o livro é instanciado o seu autor não pode ser adicionado ou removido.
+- O método toString() deve retornar a seguinte resposta:
+"Book[name=?,authors={Author[name=?,email=?,gender=?],......},price=?,qty=?]".
+
+Dicas:
+Author[] authors = new Author[2];
+authors[0] = new Author("Autor 01", "autor01@somewhere.com.br", 'm');
+authors[1] = new Author("Autor 02", "autor02@nowhere.com.br", 'm');
+
+Book testeBook = new Book("Java for Dummy", authors, 19.99, 99);
+System.out.println(testeBook); // toString()
+*/
 public class TestBook {
     public static void main(String[] args) {
         Author[] authors = new Author[2];
@@ -66,4 +84,5 @@ class Book {
         return sb.toString();
     }
 }
+
 
